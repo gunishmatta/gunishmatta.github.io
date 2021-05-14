ff809f8bf98aaa00bd2ba66404aa0dbd2b40934c
gunishmatta <33680363+gunishmatta@users.noreply.github.com>
2021-05-14 13:13:44 +0000

    /feat portfolio

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-scroll';
+import PortfolioContext from '../../context/context';
+
+const Footer = () => {
+  const { footer } = useContext(PortfolioContext);
+  const { networks } = footer;
+
+  return (
+    <footer className="footer navbar-static-bottom">
+      <Container>
+        <span className="back-to-top">
+          <Link to="hero" smooth duration={1000}>
+            <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
+          </Link>
+        </span>
+        <div className="social-links">
+          {networks &&
+            networks.map((network) => {
+              const { id, name, url } = network;
+              return (
+                <a
+                  key={id}
+                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  aria-label={name}
+                >
+                  <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
+                </a>
+              );
+            })}
+        </div>
+        <hr />
+        <p className="footer__text" />
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
